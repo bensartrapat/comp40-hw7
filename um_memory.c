9998db91a42f9d9b68d0eed282717adf56080302
bensartrapat <sartrapat_s@hotmail.com>
2015-04-14 01:17:22 -0400

    change segment0 to be its own array

--- um_memory.c
+++ um_memory.c
@@ -18,6 +18,8 @@
 #include "assert.h"
 #include "bitpack.h"
 #include "stack.h"
+#include <math.h>
+
 
 const int NUM_REGISTER = 8; 
 const int BYTESIZE = 8;
@@ -26,6 +28,7 @@ const int BYTE_UINT32 = 4; /* number of bytes in 32 bits */
 const int SHIFT_VAL = 32; 
 const int ZERO = 0;
 const int UI32SIZE = sizeof(uint32_t);
+const uint32_t NOT_ONE = ~0;
 
 static inline int get_new_ID(umMem_T memory);
 static inline void store_old_ID(umMem_T memory, int segID);
@@ -35,7 +38,7 @@ umMem_T program_init(FILE *input)
         assert(input);
         int c, progSize = 0;
         int shift = SHIFT_VAL;
-        uint64_t word = 0;
+        uint32_t word = 0;
         
         /* initialize umMem_T */
         umMem_T memory = malloc(sizeof(struct umMem_T));
@@ -55,10 +58,13 @@ umMem_T program_init(FILE *input)
         fseek(input, 0L, SEEK_SET);
         
         /* create segment zero */
-        uint32_t* data = calloc(progSize / UI32SIZE + 1, UI32SIZE);
-        data[0] = progSize;
-        *(uint32_t **)UArray_at(memory->segmentList, ZERO) = data;
-        
+//         uint32_t* data = calloc(progSize / UI32SIZE + 1, UI32SIZE);
+// //         data[0] = progSize;
+//         *(uint32_t **)UArray_at(memory->segmentList, ZERO) = data;
+
+        memory->segment0 = calloc(progSize/UI32SIZE, UI32SIZE);
+
+//         fprintf(stderr, "progSize is: %i\n", progSize/UI32SIZE);
         /* load instructions to segment zero */
         uint32_t counter = ZERO;
         while ((c = getc(input)) != EOF) {
@@ -68,8 +74,9 @@ umMem_T program_init(FILE *input)
                         					(unsigned)c);
                 }
                 if (shift == 0) {
-                        segment_put(memory, ZERO, counter, 
-                                                  (uint32_t)word);
+                        memory->segment0[counter] = word;
+//                         if (counter == 4777) fprintf(stderr, "HEY!!!!!!!!");
+//  if (counter == 4654) fprintf(stderr, "word is %"PRIu32"\n", word);
                         shift = SHIFT_VAL;
                         counter++;
                 }
@@ -91,15 +98,142 @@ void program_free(umMem_T *memory)
         }
         
         UArray_free(&((*memory)->segmentList));
+        free((*memory)->segment0);
         free((*memory)->registerList);
         Stack_free(&((*memory)->unmapStack));
         free(*memory);
 }
 
+/*----------------------------------------------------------------------------*/
+void conditional_move(umMem_T memory, int A, int B, int C)
+{
+        if (memory->registerList[C] != 0) {
+                memory->registerList[A] = memory->registerList[B];
+        }               
+}
+
+void segmented_load(umMem_T memory, int A, int B, int C)
+{
+        if (memory->registerList[B] == 0) {
+                memory->registerList[A] =
+memory->segment0[memory->registerList[C]];
+        } else {
+        uint32_t* segment = *(uint32_t**)UArray_at(memory->segmentList,
+                                        memory->registerList[B]);
+        memory->registerList[A] = segment[memory->registerList[C]+1];
+        }
+}
+
+void segmented_store(umMem_T memory, int A, int B, int C)
+{
+        segment_put(memory, memory->registerList[A],memory->registerList[B],   
+                    memory->registerList[C]);
+}
+
+void addition(umMem_T memory, int A, int B, int C)
+{     
+        memory->registerList[A] = (memory->registerList[B] +
+memory->registerList[C]);
+}
+
+void multiplication(umMem_T memory, int A, int B, int C)
+{
+        memory->registerList[A] = (memory->registerList[B] *
+memory->registerList[C]);       
+}
+
+void division(umMem_T memory, int A, int B, int C)
+{
+        assert(memory->registerList[C] != 0);
+        memory->registerList[A] = (memory->registerList[B] /
+memory->registerList[C]);
+}
+
+void bitwise_NAND(umMem_T memory, int A, int B, int C)
+{
+        memory->registerList[A] = ~(memory->registerList[B] &
+memory->registerList[C]);
+}
+
+void halt(umMem_T memory)
+{
+        program_free(&memory);
+        exit(0);
+}
+
+void map_segment(umMem_T memory, int B, int C)
+{
+//          fprintf(stderr, "in SEGMENT_MAP\n");
+        segment_map(memory, B, memory->registerList[C]);
+}
+
+void unmap_segment(umMem_T memory, int C)
+{
+        assert(memory->registerList[C] != 0);
+        segment_unmap(memory, memory->registerList[C]);
+}
+
+void output(umMem_T memory, int C)
+{
+        uint32_t toPrint = memory->registerList[C];
+        assert(toPrint <= 255);
+        putchar((int)toPrint);
+}
+
+void input(umMem_T memory, int C)
+{
+        int input = getc(stdin);
+        //assert(input >= 0 && input <= 255);
+        if (input != EOF) {
+                memory->registerList[C] = (uint32_t)input;
+        } else { 
+                memory->registerList[C] = NOT_ONE;
+        }
+}
+
+uint32_t load_program(umMem_T memory, int B, int C)
+{
+        uint32_t rB_value = memory->registerList[B];
+        
+        if (rB_value != 0) {
+//                         fprintf(stderr, "IN LOAD PROGRAM!!\n");
+//                 segment_unmap(memory, 0);
+//                 uint32_t* segment =
+// *(uint32_t**)UArray_at(memory->segmentList,
+//                                         rB_value);
+//                 int length = segment[0];
+//                 segment_map(memory, 0, length);
+                
+                /* copy from segment with ID $r[B] to segment 0 */
+//                 for (int i = 0; i < length; i++) {
+//                         uint32_t value = segment[i+1];//segment_get(memory,
+// rB_value, i);
+//                         segment_put(memory, 0, i, value);
+                free(memory->segment0);
+                uint32_t* segment =
+ *(uint32_t**)UArray_at(memory->segmentList,
+                                         rB_value);
+                int length = segment[0];
+//                 fprintf(stderr, "length %i\n", length);
+                memory->segment0 = calloc(length, UI32SIZE);
+                
+                for (int i = 0; i<length; i++) {
+                        memory->segment0[i] = segment[i+1];
+                }
+        }
+//         fprintf(stderr, "new counter is: %u\n", memory->registerList[C]);
+        return memory->registerList[C];
+}
+
+void load_value(umMem_T memory, unsigned A, unsigned value)
+{
+        memory->registerList[A] = (uint32_t)value;
+}
+/*----------------------------------------------------------------------------*/
 void segment_map(umMem_T memory, int regID, int length) 
 {
         assert(memory);
-        
+//         fprintf(stderr, "in SEGMENT_MAP\n");
         /* find an available segment ID to use */
         int newSegID = get_new_ID(memory);
         memory->registerList[regID] = newSegID;
@@ -139,20 +273,33 @@ bool segment_isEmpty(umMem_T memory, int segID)
 uint32_t segment_get(umMem_T memory, int segID, int offset)
 {
     	//assert(segment_isEmpty(memory, segID) == false);
-        uint32_t* segment = *(uint32_t **)UArray_at(memory->segmentList, segID);
+        if (segID == 0) {
+//                 fprintf(stderr, "SEGMENT_GET segID: %i, offset:"
+// "%i\n",segID,offset);
+                return memory->segment0[offset];
+        } else {
+                uint32_t* segment = *(uint32_t **)UArray_at(memory->segmentList,
+segID);
         assert(segment != NULL);
         assert(offset >= 0 && offset < (int)segment[0]);
         return segment[offset+1];
+        }
 }
 
 void segment_put(umMem_T memory, int segID, int offset, uint32_t value)
 {
         assert(memory);
-        uint32_t* segment = *(uint32_t **)UArray_at(memory->segmentList, segID);
+//         fprintf(stderr, "segID: %i, offset: %i\n",segID,offset);
+        if (segID == 0) {
+                memory->segment0[offset] = value;
+        } else {
+                uint32_t* segment = *(uint32_t **)UArray_at(memory->segmentList,
+segID);
         assert(segment != NULL);
         assert(offset >= 0 && offset < (int)segment[0]);
         
         segment[offset+1] = value;
+        }
 
 }
 
@@ -167,20 +314,6 @@ segID);
                 return 0;
         }
 }
-
-uint32_t register_get(umMem_T memory, int regID) 
-{
-	assert(memory);
-        return memory->registerList[regID];
-}
-
-
-void register_put(umMem_T memory, int regID, uint32_t value)
-{
-        assert(memory);
-        memory->registerList[regID] = value;
-}
-
 /* return an ID that can be used to map a segment
  * if unmapStack has available IDs to reuse, return an ID from that
  * else, return an ID to map segment at the end of the segmentList
--- um_memory.h~
+++ um_memory.h~
@@ -25,6 +25,7 @@ typedef struct T *T;
 struct T {
         int maxID;              /* biggest segment ID that has been mapped */
         UArray_T segmentList;   /* segmentList is a UArray of UArray */
+        uint32_t* segment0;
         uint32_t* registerList;
         Stack_T unmapStack;     /* stack stores unmapped segment IDs to reuse
                                  * later */
@@ -55,10 +56,56 @@ extern uint32_t segment_get(umMem_T memory, int segID, int offset);
 
 extern void segment_put(umMem_T memory, int segID, int offset, uint32_t value);
 
-/* get or put value in register regID */
-extern uint32_t register_get(umMem_T memory, int regID);
+/*----------------------------------------------------------------------------*/
 
-extern void register_put(umMem_T memory, int regID, uint32_t value);
+/* If the value at register C is not 0, then the value at register A is
+   the value at register B */
+extern void conditional_move(umMem_T memory, int A, int B, int C);
+
+/* Get the value stored in segment r[b], offset r[C] and store it in
+   register A */
+extern void segmented_load(umMem_T memory, int A, int B, int C);
+
+/* Put the value in register C into segment r[A], offset r[B] */
+extern void segmented_store(umMem_T memory, int A, int B, int C);
+
+/* Adds the values stored at B and C and store the result at register A */
+extern void addition(umMem_T memory, int A, int B, int C);
+
+/* Multiples the values stored at B and C and store the result at
+   register A */
+extern void multiplication(umMem_T memory, int A, int B, int C);
+
+/* Divides B with C and store the result at register A */
+extern void division(umMem_T memory, int A, int B, int C);
+
+/* Bitwise operation NAND on values stored in registers B and C and store
+   the value at register A */
+extern void bitwise_NAND(umMem_T memory, int A, int B, int C);
+
+/* Stops the computation */
+extern void halt(umMem_T memory);
+
+/* Create a segment with the given length from register C. The new
+   segment is mapped as segment r[B] */
+extern void map_segment(umMem_T memory, int B, int C);
+
+/* Segment at r[C] is unmapped */
+extern void unmap_segment(umMem_T memory, int C);
+
+/* Stdout value from register C */
+extern void output(umMem_T memory, int C);
+
+/* Stores input taken from stdin into register C */
+extern void input(umMem_T memory, int C);
+
+/* Loads a new program to segment 0 and set the program counter to the
+   value at segment 0, offset value at register C */
+extern uint32_t load_program(umMem_T memory, int B, int C);
+
+/* Stores the value (bits after 3 bits away from the opcode) at r[A] */
+extern void load_value(umMem_T memory, unsigned A, unsigned value); 
+
 
 #undef T 
 #endif 
